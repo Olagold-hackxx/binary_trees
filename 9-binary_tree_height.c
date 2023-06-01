71779464686966cfdc51a11bb030021f4ac7d435
Abdulqoyyum Aileru <Abdulqoyyum Aileru>
2023-06-01 05:53:51 -0400

    Complete mandatory tasks

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,8 +16,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		left_height = binary_tree_height(tree->left) + 1;
 	if (tree->right != NULL)
 		right_height = binary_tree_height(tree->right) + 1;
-	
-	if (left_height > right_height) 
+
+	if (left_height > right_height)
 		return (left_height);
 	else
 		return (right_height);
